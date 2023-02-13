3b576811a5c8104577ab4c54a4d6b1ad0ebd049d
mississppi <msspstudio@gmail.com>
2023-02-13 22:26:54 +0900

    init db

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,30 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use rusqlite::{params, NO_PARAMS, Connection, Result};
 
-fn main() {
+fn print_typename<T>(_: T) {
+  println!("{}", std::any::type_name::<T>());
+}
+
+pub fn init_db() -> Result<()>{
+  let path = "./ropes.db";
+  let con = Connection::open(&path)?;
+  con.execute(
+      "create table if not exists posts (
+           id integer primary key,
+           title text,
+           content text
+       )",
+      NO_PARAMS,
+  )?;
+  println!("{}", "created");
+  Ok(())
+}
+
+fn main() -> Result<()>{
+  
+  init_db();
+  
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+  Ok(())
 }
